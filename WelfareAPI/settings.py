d780d24e4032ad6830584cb748d36b562575d4d1
Sylvester Ofueu <49407516+sylvicruza@users.noreply.github.com>
2025-05-28 15:18:25 +0100

    Cors headers

--- WelfareAPI/settings.py
+++ WelfareAPI/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = ['heavensconnect.onrender.com','192.168.1.174', 'localhost', '12
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -62,6 +64,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    "https://heavensconnect-83e8c.web.app"
+]
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
